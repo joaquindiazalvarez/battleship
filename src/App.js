90c987cdaa054394250ee060389026a950860b42
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-07-02 13:54:17 +0000

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './img/logo.svg';
 import './App.css';
 import { Footer } from './components/footer';
+import { Navbar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer/>
+      <div className="container-fluid position-fixed">
+        <Navbar/>
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Footer/>
+      </div>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Navbar(){
+
+    return(
+        <div className="row" id="navbardiv">
+            <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
+                <div>
+                Let's play Battleship
+                </div>
+            </nav>
+        </div>
+    )
+}
